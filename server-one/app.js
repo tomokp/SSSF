91ab4d06b2287f7c29d0da9ac3c312bc210323d3
tomokp <tomoostkapelle@gmail.com>
2021-03-16 11:32:53 +0200

    initial commit

---
+++ server-one/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/catinfo', (req, res) => {
+  const cat = {
+    name: 'Frank',
+    age: 6,
+    weight: 5,
+  };
+  res.json(cat);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
